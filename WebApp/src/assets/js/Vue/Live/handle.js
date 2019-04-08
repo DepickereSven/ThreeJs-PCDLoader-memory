2ed2611734072166b5336bb25693812be1bfe08f
Depickere Sven <DepickereSven@users.noreply.github.com>
2019-04-08 16:22:25 +0800

    remove unneeded counter

--- WebApp/src/assets/js/Vue/Live/handle.js
+++ WebApp/src/assets/js/Vue/Live/handle.js
@@ -7,8 +7,6 @@ import Three from "./ThreeJs/Init"
 
 export default  (function () {
 
-    let counter = 1;
-
     async function initThree(container) {
         await Three.init(container);
         await Three.animate();
@@ -17,7 +15,6 @@ export default  (function () {
     async function updateThree(_self) {
         while (_self.update) {
             let newPCD = await _self.request();
-            console.log(counter++);
             if (newPCD.error){
                 _self.snack.status = true;
                 _self.snack.text = newPCD.msg;
